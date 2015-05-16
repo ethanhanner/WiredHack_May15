60e0faed9600ac8dfea6b87895a0645038ec1a2d
Joseph Neely <neelyj2@winthrop.edu>
2015-05-16 06:06:56 -0400

    lololololool

--- josephtest/script.js
+++ josephtest/script.js
@@ -74,56 +74,14 @@ $(document).ready(function() {
 		type:"GET",
 		url: "data.php",
 		dataType: 'json',
+		data: {
+			state: 'CO'	
+		},
 		success : function(data){
-			var diameter = 960,
-			    format = d3.format(",d"),
-			    color = d3.scale.category20c();
-			
-			var bubble = d3.layout.pack()
-			    .sort(null)
-			    .size([diameter, diameter])
-			    .padding(1.5);
-			
-			var svg = d3.select("body").append("svg")
-			    .attr("width", diameter)
-			    .attr("height", diameter)
-			    .attr("class", "bubble");
-			
-			d3.json(data, function(error, root) {
-			  var node = svg.selectAll(".node")
-			      .data(bubble.nodes(classes(root))
-			      .filter(function(d) { return !d.children; }))
-			    .enter().append("g")
-			      .attr("class", "node")
-			      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-			
-			  node.append("title")
-			      .text(function(d) { return d.className + ": " + format(d.value); });
-			
-			  node.append("circle")
-			      .attr("r", function(d) { return d.r; })
-			      .style("fill", function(d) { return color(d.packageName); });
-			
-			  node.append("text")
-			      .attr("dy", ".3em")
-			      .style("text-anchor", "middle")
-			      .text(function(d) { return d.className.substring(0, d.r / 3); });
-			});
-			
-			// Returns a flattened hierarchy containing all leaf nodes under the root.
-			function classes(root) {
-			  var classes = [];
-			
-			  function recurse(name, node) {
-			    if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-			    else classes.push({packageName: name, className: node.name, value: node.size});
-			  }
-			
-			  recurse(null, root);
-			  return {children: classes};
+			for(var i = 0; i<=data.length;i++){
+				var address = data[i].Address1 + " " + data[i].City + ", " + data[i].State + " " + data[i].PostalCode;
+				$('#results').append('<div class="dealerBlock"><h1>' + data[i].DealerName + '</h1>' + '<p><b>Brand: </b>' + data[i].BrandName + '</p> <p>' + address + '</p></div>');
 			}
-			
-			d3.select(self.frameElement).style("height", diameter + "px");
 		}
 	});
 	
